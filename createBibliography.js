d73c04e8d96c0a3d7188a417042a4ecc809d7544
Matt McElwee <mmcelwee@dmsi.com>
2019-08-29 21:36:59 -0500

    Added a script to create a bibliography

---
+++ createBibliography.js
@@ -0,0 +1,34 @@
+const pretty = require('pretty');
+const sort = require('sort-on');
+
+const read = require('./utils/read');
+const write = require('./utils/write');
+const generateTemplate = require('./generateTemplate');
+
+module.exports = async (srcPath, destDir) => {
+  if (!srcPath) {
+    throw new Error('Source directory path is required');
+  }
+
+  if (!destDir) {
+    throw new Error('Destination directory path is required');
+  }
+
+  const srcPathFull = path.resolve(process.cwd(), srcPath);
+  const destDirPath = path.resolve(process.cwd(), destDir);
+
+  const booksRaw = await read(srcPathFull);
+  const books = sort(JSON.parse(booksRaw), 'author');
+
+  const bibliographyTemplate = await read(path.join(__dirname, './templates/bibliography.ejs'));
+
+  const document = await generateTemplate(
+    { books },
+    bibliographyTemplate,
+    {
+      title: 'Books I\'ve Read with Commentary',
+    },
+  );
+
+  await write(path.join(destDirPath, `bibliography.html`), pretty(document));
+};
--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const copyServiceWorker = require('./copyServiceWorker');
 const createPage = require('./createPage');
 const createPost = require('./createPost');
 const compileCSS = require('./compileCSS');
+const createBibliography = require('./createBibliography');
 
 router({
   css: compileCSS,
   page: createPage,
   post: createPost,
   sw: copyServiceWorker,
+  biblio: createBibliography,
 });
